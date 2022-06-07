6e28c69ace8b03589251bd7aff03559736525957
franciscofunes <48732867+franciscofunes@users.noreply.github.com>
2022-06-06 22:58:55 -0300

    feat: search bar

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ VanillaTilt.init(document.querySelectorAll(".social__links ul li"), {
         glare: false,
         "max-glare": 0
 	});
+
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("item");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
