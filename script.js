ced50aa7c23f9cfdba166629326e47979dcf6ebf
franciscofunes <48732867+franciscofunes@users.noreply.github.com>
2022-06-13 22:17:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,22 +7,25 @@ VanillaTilt.init(document.querySelectorAll(".social__links ul li"), {
 
 
 function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
+    let input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     ul = document.getElementById("item");
     li = ul.getElementsByTagName("li");
+
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
         txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-            var p = document.createElement("p");
-            p.textContent = "No se encontraron resultados 💡";
-            document.body.appendChild(p);
+
+        } 
+        if (txtValue.toUpperCase().indexOf(filter) === 0 {
+          var p = document.createElement("p");
+          p.textContent = "No se encontraron resultados 💡";
+          document.body.appendChild(p);
         }
+        li[i].style.display = "none";
     }
 }
 
