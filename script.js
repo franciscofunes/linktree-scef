a94bea24230d48a422f04a627be8399a2ae34fb4
franciscofunes <48732867+franciscofunes@users.noreply.github.com>
2022-07-02 12:05:50 -0300

    feat: upload videos, rename function, modify link

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
-VanillaTilt.init(document.querySelectorAll(".social__links ul li"), {
-		max: 1,
-		speed: 20,
-        glare: false,
-        "max-glare": 0
-	});
+function searchLinks() {
+  let input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("item");
+  li = ul.getElementsByTagName("li");
 
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
 
-function myFunction() {
-    let input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("item");
-    li = ul.getElementsByTagName("li");
-
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = "";
-        } else {
-          li[i].style.display = "none";
-        }
-    }
-
-    if (txtValue.toUpperCase().indexOf(filter) === 0 && i === 1) {
-        var p = document.createElement("p");
-        p.textContent = "No se encontraron resultados 💡";
-        document.body.appendChild(p);
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
     } else {
-        p.style.display = "none";
+      li[i].style.display = "none";
     }
-}
+  }
 
+  if (txtValue.toUpperCase().indexOf(filter) === 0 && i === 1) {
+    var p = document.createElement("p");
+    p.textContent = "No se encontraron resultados 💡";
+    document.body.appendChild(p);
+  } else {
+    p.style.display = "none";
+  }
+}
